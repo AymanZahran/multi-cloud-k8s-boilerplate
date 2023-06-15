78b56e7abf5b3f75d539b99e7fd90d0cacc7d62f
Ayman Zahran <ah.zahran@outlook.com>
2023-06-15 23:39:03 +0300

    cdktf synth

--- cdktf.ts
+++ cdktf.ts
@@ -6,16 +6,15 @@ import {Construct} from 'constructs';
 import {AwsProvider} from '@cdktf/provider-aws/lib/provider';
 import {EksCluster} from '@cdktf/provider-aws/lib/eks-cluster';
 
-// Azure
-import {AzurermProvider} from '@cdktf/provider-azurerm/lib/provider';
-import {KubernetesCluster} from '@cdktf/provider-azurerm/lib/kubernetes-cluster';
+// // Azure
+// import {AzurermProvider} from '@cdktf/provider-azurerm/lib/provider';
+// import {KubernetesCluster} from '@cdktf/provider-azurerm/lib/kubernetes-cluster';
 
-// GCP
-import {GoogleProvider} from '@cdktf/provider-google/lib/provider';
+// // GCP
+// import {GoogleProvider} from '@cdktf/provider-google/lib/provider';
 
 // Kubernetes
 import {KubernetesProvider} from '@cdktf/provider-kubernetes/lib/provider';
-import {deployment} from '@cdktf/provider-kubernetes';
 import {Namespace} from '@cdktf/provider-kubernetes/lib/namespace';
 
 // General
@@ -27,8 +26,8 @@ config();
 interface StackConfig {
     environment: string;
     aws_region?: string;
-    azure_region?: string;
-    gcp_region?: string;
+    // azure_region?: string;
+    // gcp_region?: string;
 }
 
 class MyStack extends TerraformStack {
@@ -37,8 +36,8 @@ class MyStack extends TerraformStack {
 
         const {
             aws_region = "us-east-1",
-            azure_region = "eastus",
-            gcp_region = "us-central1",
+            // azure_region = "eastus",
+            // gcp_region = "us-central1",
         } = config;
 
         // Define Terraform Variables
@@ -47,41 +46,39 @@ class MyStack extends TerraformStack {
             default: "eks-cluster",
             description: "The name of the EKS cluster",
         });
-        const aksClusterName = new TerraformVariable(this, "aks_cluster_name", {
-            type: "string",
-            default: "aks-cluster",
-            description: "The name of the AKS cluster",
-        });
-        const gkeClusterName = new TerraformVariable(this, "gke_cluster_name", {
-            type: "string",
-            default: "gke-cluster",
-            description: "The name of the GKE cluster",
-        });
+        // const aksClusterName = new TerraformVariable(this, "aks_cluster_name", {
+        //     type: "string",
+        //     default: "aks-cluster",
+        //     description: "The name of the AKS cluster",
+        // });
+        // const gkeClusterName = new TerraformVariable(this, "gke_cluster_name", {
+        //     type: "string",
+        //     default: "gke-cluster",
+        //     description: "The name of the GKE cluster",
+        // });
 
         // Define Cloud Providers
         new AwsProvider(this, 'AWS', {
-            accessKey: process.env.AWS_ACCESS_KEY_ID || '',
-            secretKey: process.env.AWS_SECRET_ACCESS_KEY || '',
             profile: process.env.AWS_PROFILE || '',
             region: aws_region,
         });
-        new AzurermProvider(this, 'AZURE', {
-            features: {},
-            subscriptionId: process.env.AZURE_SUBSCRIPTION_ID || '',
-            tenantId: process.env.AZURE_TENANT_ID || '',
-            clientSecret: process.env.AZURE_CLIENT_SECRET || '',
-            clientId: process.env.AZURE_CLIENT_ID || '',
-        });
-        new GoogleProvider(this, 'GOOGLE', {
-            region: process.env.GOOGLE_REGION || '',
-            project: process.env.GOOGLE_PROJECT || '',
-            zone: process.env.GOOGLE_ZONE || '',
-        });
+        // new AzurermProvider(this, 'AZURE', {
+        //     features: {},
+        //     subscriptionId: process.env.AZURE_SUBSCRIPTION_ID || '',
+        //     tenantId: process.env.AZURE_TENANT_ID || '',
+        //     clientSecret: process.env.AZURE_CLIENT_SECRET || '',
+        //     clientId: process.env.AZURE_CLIENT_ID || '',
+        // });
+        // new GoogleProvider(this, 'GOOGLE', {
+        //     region: process.env.GOOGLE_REGION || '',
+        //     project: process.env.GOOGLE_PROJECT || '',
+        //     zone: process.env.GOOGLE_ZONE || '',
+        // });
 
         // Create an EKS cluster
         const eks_cluster = new EksCluster(this, 'eks_cluster', {
             name: eksClusterName.value,
-            version: '1.17',
+            version: '1.24',
             roleArn: 'arn:aws:iam::123456789012:role/eks-cluster-role',
             vpcConfig: {
                 subnetIds: ['subnet-abcde012', 'subnet-bcde012a', 'subnet-fghi345a'],
@@ -100,68 +97,29 @@ class MyStack extends TerraformStack {
 
 
 
-        const aks_cluster = new KubernetesCluster(this, 'aks_cluster', {
-            name: aksClusterName.value,
-            location: azure_region,
-            resourceGroupName: 'kubernetes',
-            defaultNodePool: {
-                name: 'default',
-                vmSize: 'Standard_D2_v2',
-            },
-            tags: {
-                environment: config.environment,
-            }
-        });
+        // const aks_cluster = new KubernetesCluster(this, 'aks_cluster', {
+        //     name: aksClusterName.value,
+        //     location: azure_region,
+        //     resourceGroupName: 'kubernetes',
+        //     defaultNodePool: {
+        //         name: 'default',
+        //         vmSize: 'Standard_D2_v2',
+        //     },
+        //     tags: {
+        //         environment: config.environment,
+        //     }
+        // });
+        //
+        // const aks_provider = new KubernetesProvider(this, 'AKS_KUBERNETES', {
+        //     host: aks_cluster.id,
+        //     clusterCaCertificate: aks_cluster.kubeConfigRaw,
+        // });
 
-        const aks_provider = new KubernetesProvider(this, 'AKS_KUBERNETES', {
-            host: aks_cluster.id,
-            clusterCaCertificate: aks_cluster.kubeConfigRaw,
-        });
 
 
-
-        const fluxNamespace = new Namespace(this, 'flux-namespace', {
+        new Namespace(this, 'flux-namespace', {
             metadata: {
                 name: 'flux-system',
-            },
-        });
-
-        const app = 'nginx-example';
-        new deployment.Deployment(this, 'nginx-deployment', {
-            metadata: {
-                name: app,
-                namespace: fluxNamespace.metadata.name,
-                labels: {
-                    app,
-                },
-            },
-            spec: {
-                replicas: '1',
-                selector: {
-                    matchLabels: {
-                        app,
-                    },
-                },
-                template: {
-                    metadata: {
-                        labels: {
-                            app,
-                        },
-                    },
-                    spec: {
-                        container: [
-                            {
-                                image: 'nginx:1.7.8',
-                                name: 'example',
-                                port: [
-                                    {
-                                        containerPort: 80,
-                                    },
-                                ],
-                            },
-                        ],
-                    },
-                },
             },
         });
 
@@ -170,18 +128,18 @@ class MyStack extends TerraformStack {
             value: eks_provider.clientKey,
             description: 'The EKS provider key',
         });
-        new TerraformOutput(this, 'aks_provider_key', {
-            value: aks_provider.clientKey,
-            description: 'The AKS provider key',
-        });
-        new TerraformOutput(this, 'gcp_region', {
-            value: gcp_region,
-            description: 'The GCP region',
-        });
-        new TerraformOutput(this, 'gcp_cluster_name', {
-            value: gkeClusterName.value,
-            description: 'The GKE cluster name',
-        });
+        // new TerraformOutput(this, 'aks_provider_key', {
+        //     value: aks_provider.clientKey,
+        //     description: 'The AKS provider key',
+        // });
+        // new TerraformOutput(this, 'gcp_region', {
+        //     value: gcp_region,
+        //     description: 'The GCP region',
+        // });
+        // new TerraformOutput(this, 'gcp_cluster_name', {
+        //     value: gkeClusterName.value,
+        //     description: 'The GKE cluster name',
+        // });
     }
 }
 
@@ -191,28 +149,28 @@ const app = new App();
 const devStack = new MyStack(app, "dev", {
     environment: "dev",
     aws_region: "us-east-2",
-    azure_region: "eastus2",
-    gcp_region: "us-central1",
+    // azure_region: "eastus2",
+    // gcp_region: "us-central1",
 });
 const stagingStack =new MyStack(app, "staging", {
     environment: "staging",
     aws_region: "us-east-2",
-    azure_region: "eastus2",
-    gcp_region: "us-central1",
+    // azure_region: "eastus2",
+    // gcp_region: "us-central1",
 });
 const prodStack =new MyStack(app, "prod", {
     environment: "prod",
     aws_region: "us-east-2",
-    azure_region: "eastus2",
-    gcp_region: "us-central1",
+    // azure_region: "eastus2",
+    // gcp_region: "us-central1",
 });
 
 for (const stack of [devStack, stagingStack, prodStack]) {
     new RemoteBackend(stack, {
         hostname: "app.terraform.io",
-        organization: "cdktf",
+        organization: "Ayman-Organization",
         workspaces: {
-            name: stack.node.id,
+            name: "multi-cloud",
         }
     })
 }
