bb466dd90cd551f8c012cfce9b172e597fd1e183
Ayman Zahran <ah.zahran@outlook.com>
2023-07-14 18:56:26 +0300

    fix-prbs

--- src/cdk8s/consul.ts
+++ src/cdk8s/consul.ts
@@ -1,16 +1,16 @@
-import {Helm, Chart, ChartProps} from 'cdk8s';
-import {Construct} from 'constructs';
+import { Helm, Chart, ChartProps } from 'cdk8s';
+import { Construct } from 'constructs';
 
 export class Consul extends Chart {
-    constructor(scope: Construct, id: string, props: ChartProps = {}) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props: ChartProps = {}) {
+    super(scope, id, props);
 
-        new Helm(this, 'consul', {
-            chart: 'hashicorp/consul',
-            version: '1.1.2',
-            namespace: 'consul',
-            releaseName: 'consul',
-            values: {}
-        });
-    }
+    new Helm(this, 'consul', {
+      chart: 'hashicorp/consul',
+      version: '1.1.2',
+      namespace: 'consul',
+      releaseName: 'consul',
+      values: {},
+    });
+  }
 }
--- src/cdk8s/vault.ts
+++ src/cdk8s/vault.ts
@@ -1,26 +1,26 @@
-import {Helm, Chart, ChartProps} from 'cdk8s';
-import {Construct} from 'constructs';
+import { Helm, Chart, ChartProps } from 'cdk8s';
+import { Construct } from 'constructs';
 
 export class Vault extends Chart {
-    constructor(scope: Construct, id: string, props: ChartProps = {}) {
-        super(scope, id, props);
+  constructor(scope: Construct, id: string, props: ChartProps = {}) {
+    super(scope, id, props);
 
-        // Storage Backend
-        new Helm(this, 'consul', {
-            chart: 'hashicorp/consul',
-            version: '1.1.2',
-            namespace: 'consul',
-            releaseName: 'consul',
-            values: {}
-        });
+    // Storage Backend
+    new Helm(this, 'consul', {
+      chart: 'hashicorp/consul',
+      version: '1.1.2',
+      namespace: 'consul',
+      releaseName: 'consul',
+      values: {},
+    });
 
-        // Vault
-        new Helm(this, 'vault', {
-            chart: 'hashicorp/vault',
-            version: '0.25.0',
-            namespace: 'vault',
-            releaseName: 'vault',
-            values: {}
-        });
-    }
+    // Vault
+    new Helm(this, 'vault', {
+      chart: 'hashicorp/vault',
+      version: '0.25.0',
+      namespace: 'vault',
+      releaseName: 'vault',
+      values: {},
+    });
+  }
 }
