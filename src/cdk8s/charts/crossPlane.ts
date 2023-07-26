c3e196347cf94a2a733bcdab417f4d7fa8a8eccf
Ayman Zahran <ah.zahran@outlook.com>
2023-07-26 17:59:14 +0300

    add-crossplane-cloud-providers

--- src/cdk8s/charts/crossPlane.ts
+++ src/cdk8s/charts/crossPlane.ts
@@ -1,24 +1,43 @@
-import { Helm, Chart, ChartProps } from "cdk8s";
-import { Construct } from "constructs";
+import {Helm, Chart, ChartProps, ApiObject} from "cdk8s";
+import {Construct} from "constructs";
 
 export class CrossPlane extends Chart {
-  constructor(
-    scope: Construct,
-    id: string,
-    props: ChartProps,
-    helmFlags?: string[],
-    version?: string,
-    values?: any,
-  ) {
-    super(scope, id, props);
+    constructor(
+        scope: Construct,
+        id: string,
+        props: ChartProps,
+        helmFlags?: string[],
+        version?: string,
+        values?: any,
+    ) {
+        super(scope, id, props);
 
-    new Helm(this, "crossplane", {
-      chart: "crossplane-stable/crossplane",
-      releaseName: "crossplane",
-      namespace: "crossplane-system",
-      helmFlags: helmFlags,
-      version: version,
-      values: values,
-    });
-  }
+        new Helm(this, "crossplane", {
+            chart: "crossplane-stable/crossplane",
+            releaseName: "crossplane",
+            namespace: "crossplane-system",
+            helmFlags: helmFlags,
+            version: version,
+            values: values,
+        });
+
+        new ApiObject(this, "crossplane-provider", {
+            apiVersion: "aws.crossplane.io/v1beta1",
+            kind: "ProviderConfig",
+            metadata: {
+                name: "default",
+            },
+            spec: {
+                credentials: {
+                    source: {
+                        secretRef: {
+                            namespace: "crossplane-system",
+                            name: "admin-key",
+                            key: "admin-secret-key",
+                        },
+                    },
+                },
+            },
+        });
+    }
 }
